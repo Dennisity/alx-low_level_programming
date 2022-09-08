26432bf68a8585209ed4cad0405611c55d68993b
root <dennisity7@gmail.com>
2022-09-08 10:28:23 -0700

    pritf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print open sesame
+ *
+ * Return: Always 0 (great work)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
