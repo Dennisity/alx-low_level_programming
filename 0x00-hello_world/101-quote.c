7c30b20003076b24d4352a77cf94ce349cb96239
root <dennisity7@gmail.com>
2022-09-08 11:12:26 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h> 
+/**
+ * main - print open sesame
+ *
+ * Return: Always 1 great work
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
