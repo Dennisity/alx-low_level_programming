727f8cbedfc6433b977719e542606f8b3763b396
root <dennisity7@gmail.com>
2022-09-09 07:03:07 -0700

    6siz

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,14 @@
  *
  * Return: Always 0 great work
  */
-int main(void)
+int main()
 {
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-printf("Size of a char: %d byte(s)\n", sizeof(a));
-printf("Size of an int: %d byte(s)\n", sizeof(b));
-printf("Size of a long int: %d byte(s)\n", sizeof(c));
-printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-printf("Size of an float: %d byte(s)\n", sizeof(e));
+int x;
+char z;
+printf( "The size of a char: %d byte(s)\n", sizeof(char));
+printf( "The size of an int: %d byte(s)\n", sizeof(int));
+printf( "The size of a long int: %d byte(s)\n", sizeof(long int));
+printf( "The size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf( "The size of an float: %d byte(s)\n", sizeof(float));
 return (0);
 }
