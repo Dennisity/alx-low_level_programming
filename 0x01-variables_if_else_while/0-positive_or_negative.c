d9e5cfb246e4b44b9ed3aeff806feb05dabe2d7d
root <dennisity7@gmail.com>
2022-09-09 07:32:26 -0700

    negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - open sesame point
  *
@@ -10,24 +9,19 @@
 int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
-IF (n == 0)
+if (n == 0)
 {
 printf("%i is zero\n", n);
 }
-
 else if (n < 0)
 {
 printf("%i is negative\n", n);
 }
-
 else
 {
 printf("%i is positive\n", n);
 }
-
 return (0);
 }
