869abb98f950db0d1f730441287d60b56e508313
root <dennisity7@gmail.com>
2022-09-13 13:31:23 -0700

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,13 @@
 /**
  * print_last_digit - prints last digit
  * @n; the number in question
+ *
  * Return: value of the last digit.
  */
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
+
 	if (last_digit < 0)
 		last_digit *= -1;
 
