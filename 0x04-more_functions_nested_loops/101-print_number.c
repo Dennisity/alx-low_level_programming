a7e915efd0940e727c353dc3ea4d45280db66443
root <dennisity7@gmail.com>
2022-09-15 10:36:28 -0700

    t121

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * print_number - Prints a number
  * @n: The number to print
@@ -8,7 +9,6 @@
 void print_number(int n)
 {
 	unsigned int num = n;
-
 	if (n < 0)
 	{
 		putchar('-');
